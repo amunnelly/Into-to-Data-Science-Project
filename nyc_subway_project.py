8d5cfa1facf8ebb4236873056bc09cd6a4e1b5f4
amunnelly <anthonymunnelly@Anthony-Munnellys-MacBook-Pro.local>
2015-07-03 23:26:50 +0100

    Edits and Corrections
    Edits and corrections made to .pdf document
    Two new functions added to the code, a Mann-Whitney U Test function and
    a function to plot mean temperature by date.
    Student’s t-test function removed.
    OLS function amended to remove EXITSn_hourly as a factor.

--- nyc_subway_project.py
+++ nyc_subway_project.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 13:45:52 2015
+
+@author: anthonymunnelly
+"""
 
 import pandas as pd
+import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import scipy.stats
 import statsmodels.api as sm
 
-url_nyc = "turnstile_data_master_with_weather.csv"
+url_nyc = "/Users/anthonymunnelly/Documents/Udacity/Intro to Data Science/NYC Subway Project/turnstile_data_master_with_weather.csv"
+
 nyc = pd.read_csv(url_nyc)
+
+def date_to_day(somedate):
+    return somedate.strftime('%A')
 
     
 def workday_holiday(somedate):
-    '''
-    (datetime) -> int
-    Examine the somedate datetime variable to see if it's a Saturday or
-    Sunday. Return 0 if it is, 1 if it's not.
-    '''
     if somedate.strftime('%A') == 'Saturday' or somedate.strftime('%A') == 'Sunday':
         return 0
     else:
         return 1
-
 
 def tidy_up_dates(nyc):
     nyc['DATEn'] = pd.to_datetime(nyc.DATEn, "%Y-%m-%d")
@@ -31,56 +35,55 @@ def tidy_up_dates(nyc):
 
 
 def normalise(data):
-    '''
-    (pd.Series) -> pd.Series
-    Take a pd.Series data and returns that series normalised to having
-    a mean of 0 and a standard deviation of 1.
-    '''
     mean = data.mean()
     sd = data.std()
     return (data-mean)/sd
 
 
-def ttest(nyc):
-    '''
-    (pd.DataFrame) -> tuple
-    Take a Dataframe and returns the t-value and p-value of the ENTRIESn_hourly
-    for that Dataframe, comparing a series of entries on rainy days and 
-    dry days.
-    '''
+def mann_whitney_test(nyc):
     rain = nyc[nyc.rain == 1]
     shine = nyc[nyc.rain == 0]
-    print "Mean Rain: {}".format(rain.ENTRIESn_hourly.mean())
-    print "Mean Shine: {}".format(shine.ENTRIESn_hourly.mean())
     
-    return scipy.stats.ttest_ind(rain.ENTRIESn_hourly, shine.ENTRIESn_hourly, equal_var = False)
+    return scipy.stats.mannwhitneyu(rain.ENTRIESn_hourly, shine.ENTRIESn_hourly)
 
 
-def calculate_OLS_features(nyc):
+def plot_mean_temperatures(nyc):
+    fig=plt.figure(1)
+    plt.plot(nyc.meantempi, color = 'red')
+    dates = pd.date_range('2011-05-01', '2011-05-30', freq = '2D')
+    #Create date labels
+    date_labels = [d.strftime('%a-%d') for d in dates]
+    # Arrange the date labels so they appear correctly on the graph - harder than it looks
+    plt.xticks(np.arange(0, 131951, 131951/15), date_labels, rotation=45)
+    plt.title('Mean Temperature by Day')
+    plt.xlabel('Day')
+    plt.ylabel('Temperatures')
+    plt.show()
+    fig.savefig('mean_temperature_by_day.png')
+    return
+
+
+def calculate_OLS(nyc):
     numerical_data = nyc[["meanpressurei",
-                          "EXITSn_hourly",
                           "meantempi",
                           "fog",
                           "precipi",
                           "rain"]]
                           
     features = normalise(numerical_data)
+    
     day_type = pd.get_dummies(nyc.Day_Type)
     features = features.join(day_type)
+
     features.corr().to_csv('correlation_checker.csv')
-        
+    
     return features
 
 
 def plotting_for_report(nyc):
-    '''
-    (pd.DataFrame) -> None
-    Plots histograms for ENTRIESn_hourly on rainy and dry days.
-    '''
     rain = nyc[nyc.rain == 1]
     shine = nyc[nyc.rain == 0]
     data = [rain.ENTRIESn_hourly, shine.ENTRIESn_hourly]
-
     fig, axes = plt.subplots(2,1, sharex = True, sharey = True)
     plt.title('Entries per Hour for Rainy and Dry Days')
     fig.subplots_adjust(hspace = 0.5)
@@ -98,11 +101,9 @@ def plotting_for_report(nyc):
     
     return
 
-
 def plot_entries_v_date(nyc):
     fig = plt.figure(1)    
-    plt.plot_date(nyc.DATEn, nyc.ENTRIESn_hourly, alpha = 0.5)
-#    dates = pd.date_range('2011-05-01', '2011-05-30', freq = 'D')
+    plt.plot_date(nyc.DATEn, nyc.ENTRIESn_hourly.sum(), 'b-')
     fig.autofmt_xdate()
     plt.xlabel('Date')
     plt.ylabel('Ridership')
@@ -110,10 +111,10 @@ def plot_entries_v_date(nyc):
     fig.savefig('entries_v_date.png')
     
     return
-    
-    
+        
 if __name__ == '__main__':
     nyc = tidy_up_dates(nyc)
-    print ttest(nyc)
-    features = calculate_OLS_features(nyc)
+    print mann_whitney_test(nyc)
+    plot_mean_temperatures(nyc)
+    features = calculate_OLS(nyc)
     print sm.OLS(nyc.ENTRIESn_hourly, features).fit().summary()
